10b36c65a06a68edd58daa67004da4d4a0f4e951
shuklaaryan <shuklaaryan34@gmail.com>
2025-02-16 19:49:10 +0530

    first commit

---
+++ js-wt-3.js
@@ -0,0 +1,13 @@
+// Program to create a function that return thre reverse of a string
+
+// function reverse(str) {
+//     let rev = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         rev+= str[i];
+//     }
+//     return rev;
+// }
+// console.log(reverse("Palindrome"));
+// console.log(reverse("Jai Shree Ram"));
+// console.log(reverse("Geekster"));
+
